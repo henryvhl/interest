ccb200fc7403274e56ad317f51a3a317ec949da6
henryvhl <henry.v.h.l@gmail.com>
2016-11-29 22:31:17 -0800

    Add verbose flag to 'printTopic' method

--- src/CountAppearance.java
+++ src/CountAppearance.java
@@ -21,30 +21,30 @@ public class CountAppearance {
     };
     
     public static void main(String[] args) throws IOException {
-        List<ChatRecord> records = readRecord("20161128.txt");
+        List<ChatRecord> records = readRecord("20161129.txt");
         System.out.println("Total records: " + records.size());
         System.out.println();
-        for (ChatRecord record : records) {
-            System.out.println(record);
-        }
-        System.out.println();
+//        for (ChatRecord record : records) {
+//            System.out.println(record);
+//        }
+//        System.out.println();
         
         List<Participant> people = countParticipants(records);
         System.out.println("Total participants: " + people.size());
         Collections.sort(people, COMPARE_BY_RECORDS.reversed());
-//        System.out.println();
-//        for (Participant person : people) {
-//            System.out.println(person);
-//        }
+        System.out.println();
+        for (Participant person : people) {
+            System.out.println(person);
+        }
         System.out.println("\nThe most active participant: " + people.get(0).getName());
         people.get(0).printRecords();
         System.out.println();
         
         String[] topics = {"\u4f5c\u4e1a", "\u62db\u8058", "project", 
-                "\u8bfe\u7a0b", "简历", "面试", "老师", "java", "春", "冬"}; 
+                "\u8bfe\u7a0b", "简历", "面试", "老师", "java", "666", "+1"}; 
         
         for (int i = 0; i < topics.length; i++) {
-            printTopic(records, i+1, topics[i]);
+            printTopic(records, i+1, topics[i], topics[i] == "1");
         }
 //        System.out.println(String.format("\\u%04x\\u%04x", (int)'课', (int)'次'));
     }
@@ -66,13 +66,19 @@ public class CountAppearance {
             int index_leftP = line.indexOf('(');
             String name = line.substring(prefix.length(), index_to);
             String timeAsString = line.substring(index_leftP + 1, line.length() - suffix.length());
-            ChatRecord record = new ChatRecord(name, timeAsString, formatter);
-            records.add(record);
-            
-            line = reader.readLine();
-            while (line != null && !line.startsWith(prefix)) {
-                record.addLine(line);
+            try {
+                ChatRecord record = new ChatRecord(name, timeAsString, formatter);
+                records.add(record);
+                
                 line = reader.readLine();
+                while (line != null && !line.startsWith(prefix)) {
+                    record.addLine(line);
+                    line = reader.readLine();
+                }
+            } catch (java.time.format.DateTimeParseException e) {
+                System.out.println(name + ": " + timeAsString + ": " + line);
+                reader.close();
+                throw e;
             }
         }
         reader.close();
@@ -97,13 +103,15 @@ public class CountAppearance {
         return new ArrayList<Participant>(nameMap.values());
     }
     
-    private static void printTopic(List<ChatRecord> records, int index, String topic) {
+    private static void printTopic(List<ChatRecord> records, int index, String topic, boolean verbose) {
         int count = 0;
         StringBuffer buffer = new StringBuffer();
         for (ChatRecord record : records) {
             if (record.getWords().contains(topic)) {
                 count ++;
-                //buffer.append(record).append('\n');
+                if (verbose) {
+                    buffer.append(record).append('\n');
+                }
             }
         }
         System.out.println("[Topic "+ index +"] '" + topic + "': "+ count +"\u6b21");
